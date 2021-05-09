d3806451791751abba79879d6f32230f014f8187
Ricardas Pukelis <ricardas.pukelis@gmail.com>
2021-05-09 11:56:08 +0300

    Scaffolded initial API structure

---
+++ LiftManagementApi/Controllers/LiftsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiftManagementApi.Controllers
+{
+    [ApiController]
+    public class LiftsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IList<LiftDto>>> Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IList<LiftDto>>> Index([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public async Task<ActionResult> Call([FromRoute] int id, CallLiftDto callLiftDto)
+        {
+            return Ok();
+        }
+    }
+}
